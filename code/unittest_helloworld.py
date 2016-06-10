124556cf12f66bdc9a217805c16e6fa535acd1bf
Josh Phillips <joshua.andrew.phillips@gmail.com>
2016-06-10 16:03:35 -0400

    modified fabfile for precommit

--- code/unittest_helloworld.py
+++ code/unittest_helloworld.py
@@ -2,11 +2,11 @@ import unittest
 
 
 class TestStringMethods(unittest.TestCase):
-	'''This is our test class'''
-	
-	def test_print(self):
-		'''test helloworld'''
-		import helloworld
-		
+    '''This is our test class'''
+
+    def test_print(self):
+        '''test helloworld'''
+        import helloworld
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- fabfile.py
+++ fabfile.py
@@ -17,3 +17,13 @@ def run_tests():
     _ensure_virtualenv()
     local('python code/unittest_helloworld.py')
 
+def run_pep8():
+    _ensure_virtualenv()
+    local('pep8 code')
+
+def pre_commit():
+    _ensure_virtualenv()
+    install_deps()
+    run_pep8()
+    run_tests()
+    
