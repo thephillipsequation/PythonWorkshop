f00ae1a0fe4a33fc7b62c9f77e683637b27d3f45
Semenov, Victor (5AM Solutions) <semenovv@MedImmune.com>
2016-09-02 11:01:59 -0400

    git sucks

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ import sys
 import os
 from fabric.api import env, local, sudo, require, run, put, settings, cd, lcd
 
+
 def _ensure_virtualenv():
     if "VIRTUAL_ENV" not in os.environ:
         sys.stderr.write("$VIRTUAL_ENV not found. Make sure to activate virtualenv first\n\n")
@@ -13,19 +14,25 @@ def install_deps():
     _ensure_virtualenv()
     local('pip install -q -r dependencies/dependencies.txt' % env)
 
+
 def run_tests():
     _ensure_virtualenv()
-    local('nosetests test')
+    local('rm -rf reports')
+    local('mkdir -p reports')
+    local('nosetests test --cover-min-percentage=100 --with-coverage --cover-package=code --cover-html --cover-html-dir=reports --cover-branches')
+
 
 def run_pep8():
     _ensure_virtualenv()
     local('pep8 code')
     local('pep8 test')
 
+
 def run_pylint():
     _ensure_virtualenv()
     local('pylint code --rcfile=dev_conf/pylintrc.txt')
     local('pylint test --rcfile=dev_conf/pylintrc.txt')
+
 
 def pre_commit():
     _ensure_virtualenv()
